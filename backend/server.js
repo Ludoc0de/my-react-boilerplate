201cd22540eaa1a21ebd51ecb3f7f4b0b8c67403
ludoc0de <champi.ludo@tutanota.com>
2024-05-30 11:43:05 -0300

    init server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log("Not again");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Server is running on ${process.env.PORT}!`)
+);
