01fb46a5821d59bdb316f427b12ac0394f4e1da3
ludoc0de <champi.ludo@tutanota.com>
2024-05-30 23:11:01 -0300

    available website route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const websiteRoutes = require("./routes/websiteRoutes");
 const app = express();
+
+//Set Routes
+app.use("/api/web", websiteRoutes);
 
 app.listen(process.env.PORT || 5000, () =>
   console.log(`Server is running on ${process.env.PORT}!`)
