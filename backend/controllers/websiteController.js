137b12700dfd708172582663dd178718d3d16789
ludoc0de <champi.ludo@tutanota.com>
2024-05-31 17:57:05 -0300

    available CRUD website

--- backend/controllers/websiteController.js
+++ backend/controllers/websiteController.js
@@ -6,28 +6,53 @@ const Website = require("../models/websiteModel");
 // Route GET /api/web
 // Aces Public
 const getWebsite = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get web!!!" });
+  const websites = await Website.find();
+
+  res.status(200).json(websites);
 });
 
 // Post website
 // Route Post /api/web
 // Aces Private
 const postWebsite = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get web!!!" });
+  const websites = await Website.create({
+    text: req.body.text,
+  });
+  res.status(200).json(websites);
 });
 
 // Update website
 // Route Put /api/web/:id
 // Aces Private
 const updateWebsite = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update web ${req.params.id}` });
+  const website = await Website.findById(req.params.id);
+  if (!website) {
+    res.status(400);
+    throw new Error("website was not found");
+  }
+
+  const updatedWebsite = await Website.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
+  res.status(200).json(updatedWebsite);
 });
 
 // Delete website
 // Route Delete /api/web/:id
 // Aces Private
 const deleteWebsite = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `del web ${req.params.id}` });
+  const website = await Website.findById(req.params.id);
+  if (!website) {
+    res.status(400);
+    throw new Error("website was not found");
+  }
+
+  await website.deleteOne();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
