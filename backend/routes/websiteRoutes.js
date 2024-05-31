8b8d3c16c8228c9a533ec599b15946c159e17708
ludoc0de <champi.ludo@tutanota.com>
2024-05-31 16:55:03 -0300

    just up const router

--- backend/routes/websiteRoutes.js
+++ backend/routes/websiteRoutes.js
@@ -1,12 +1,12 @@
 //Web routes
 const express = require("express");
+const router = express.Router();
 const {
   getWebsite,
   postWebsite,
   updateWebsite,
   deleteWebsite,
 } = require("../controllers/websiteController");
-const router = express.Router();
 
 router.get("/", getWebsite);
 router.post("/", postWebsite);
