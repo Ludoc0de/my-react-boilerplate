cb92da4b8217aa13d87caf47c36cbd78c707f043
ludoc0de <champi.ludo@tutanota.com>
2024-05-31 16:53:42 -0300

    add error handle

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const websiteRoutes = require("./routes/websiteRoutes");
 const app = express();
 
+//set middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //Set Routes
 app.use("/api/web", websiteRoutes);
+
+//to overwrite default express error
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 5000, () =>
   console.log(`Server is running on ${process.env.PORT}!`)
