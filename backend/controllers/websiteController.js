f2d10baebcd2149bfb11fad64019d3cf15c0f638
ludoc0de <champi.ludo@tutanota.com>
2024-05-31 16:54:28 -0300

    add async handler

--- backend/controllers/websiteController.js
+++ backend/controllers/websiteController.js
@@ -1,30 +1,33 @@
+// asyncHandler to use errorHandler and avoid try/catch
+const asyncHandler = require("express-async-handler");
+
 // Get website
 // Route GET /api/web
 // Aces Public
-const getWebsite = (req, res) => {
+const getWebsite = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get web!!!" });
-};
+});
 
 // Post website
 // Route Post /api/web
 // Aces Private
-const postWebsite = (req, res) => {
+const postWebsite = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get web!!!" });
-};
+});
 
 // Update website
 // Route Put /api/web/:id
 // Aces Private
-const updateWebsite = (req, res) => {
+const updateWebsite = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `update web ${req.params.id}` });
-};
+});
 
 // Delete website
 // Route Delete /api/web/:id
 // Aces Private
-const deleteWebsite = (req, res) => {
+const deleteWebsite = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `del web ${req.params.id}` });
-};
+});
 
 module.exports = {
   getWebsite,
