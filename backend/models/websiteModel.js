b57250ef62a65694728995054e57a060954da23a
ludoc0de <champi.ludo@tutanota.com>
2024-06-01 16:23:58 -0300

    associate user to website

--- backend/models/websiteModel.js
+++ backend/models/websiteModel.js
@@ -3,6 +3,12 @@ const mongoose = require("mongoose");
 
 const websiteSchema = new mongoose.Schema(
   {
+    //associate user to website
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      require: true,
+      ref: "User",
+    },
     text: {
       type: String,
       required: true,
