6630455dc174de7a4f80801d4714ab171d6b7dcf
ludoc0de <champi.ludo@tutanota.com>
2024-06-03 23:10:06 -0300

    fetch data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [website, setWebsite] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/users/user")
+      .then((response) => {
+        setWebsite(response);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(website);
+
+  return (
+    <div>
+      <div>Home</div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div>
+          {website.map((web) => (
+            <p key={web._id}>{web.text}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
