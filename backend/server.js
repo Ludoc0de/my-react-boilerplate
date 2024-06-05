dd9cfccb561c33f60111bee306e1ca2d0190b981
ludoc0de <champi.ludo@tutanota.com>
2024-06-04 22:09:01 -0300

    require cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const colors = require("colors");
+const cors = require("cors");
 const websiteRoutes = require("./routes/websiteRoutes");
 const userRoutes = require("./routes/userRoutes");
 const dotenv = require("dotenv").config();
