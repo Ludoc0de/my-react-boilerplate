8b8fdf1269ff862929e1f8d18b3283814fcd44c5
ludoc0de <champi.ludo@tutanota.com>
2024-06-08 10:03:44 -0300

    add logout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import authService from "../features/authService";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    authService.logout();
+    navigate("/login");
+  };
+
   return (
     <div className="grid">
       <div>Home</div>
       <Link to="/login">Login</Link>
+      <button onClick={handleLogout}>Logout</button>
       <Link to="/websites">Websites</Link>
     </div>
   );
