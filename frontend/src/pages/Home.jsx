eb6fdd7724b1f780ee71a1ab258ec03efc51021a
ludoc0de <champi.ludo@tutanota.com>
2024-06-08 08:30:36 -0300

    getWebsite through axios

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,29 +1,45 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 // import { Link } from "react-router-dom";
 
 const Home = () => {
   const [website, setWebsite] = useState([]);
-  console.log(website);
+  // console.log(website);
   const [loading, setLoading] = useState(false);
+  const API_URL = "http://localhost:5000/api/web/";
+  // Get website
+  const getWebsite = async (token) => {
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const response = await axios.get(API_URL, config);
+      setWebsite(response.data);
+      // return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    axios
-      .get("http://localhost:5000/api/web/")
-      .then((response) => {
-        setWebsite(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user && user.token) {
+      getWebsite(user.token);
+    } else {
+      setLoading(false);
+    }
   }, []);
 
   return (
     <div>
       <div>Home</div>
-      {/* {loading ? (
+      <Link to="/login">Login</Link>
+      {loading ? (
         <Spinner />
       ) : (
         <div>
@@ -31,7 +47,7 @@ const Home = () => {
             <p key={web._id}>{web.text}</p>
           ))}
         </div>
-      )} */}
+      )}
     </div>
   );
 };
