cc82e728497283f587ad805e1e6c14c74fc5d7bc
ludoc0de <champi.ludo@tutanota.com>
2024-06-08 09:24:44 -0300

    move websites to component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,53 +1,11 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
-import Spinner from "../components/Spinner";
-// import { Link } from "react-router-dom";
 
 const Home = () => {
-  const [website, setWebsite] = useState([]);
-  // console.log(website);
-  const [loading, setLoading] = useState(false);
-  const API_URL = "http://localhost:5000/api/web/";
-  // Get website
-  const getWebsite = async (token) => {
-    try {
-      const config = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-
-      const response = await axios.get(API_URL, config);
-      setWebsite(response.data);
-      // return response.data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem("user"));
-    if (user && user.token) {
-      getWebsite(user.token);
-    } else {
-      setLoading(false);
-    }
-  }, []);
-
   return (
-    <div>
+    <div className="grid">
       <div>Home</div>
       <Link to="/login">Login</Link>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <div>
-          {website.map((web) => (
-            <p key={web._id}>{web.text}</p>
-          ))}
-        </div>
-      )}
+      <Link to="/websites">Websites</Link>
     </div>
   );
 };
