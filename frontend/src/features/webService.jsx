5a1c6a3fcd5ba98f19f002a4f1b480887555a23e
ludoc0de <champi.ludo@tutanota.com>
2024-06-09 00:07:17 -0300

    try-catch

--- frontend/src/features/webService.jsx
+++ frontend/src/features/webService.jsx
@@ -3,49 +3,62 @@ const API_URL = "http://localhost:5000/api/web/";
 
 // Create website
 const createWebsite = async (webData, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  const response = await axios.post(API_URL, webData, config);
-  return response.data;
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.post(API_URL, webData, config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // Get Website
 const getWebsite = async (token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-
-  const response = await axios.get(API_URL, config);
-  return response.data;
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.get(API_URL, config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // Delete Website
 const deleteWebsite = async (id, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-
-  const response = await axios.delete(API_URL + id, config);
-  return response.data;
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.delete(API_URL + id, config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // Update Website
 const updateWebsite = async (id, profileData, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-
-  const response = await axios.put(API_URL + id, profileData, config);
-  return response.data;
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.put(API_URL + id, profileData, config);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // export webService
